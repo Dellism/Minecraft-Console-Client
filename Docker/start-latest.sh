4c298836f51af58fd22a497ea2911bb40ba270bf
Dellism <mostsity81@gmail.com>
2024-10-31 00:14:24 +0200

    changed something

--- Docker/start-latest.sh
+++ Docker/start-latest.sh
@@ -8,7 +8,7 @@ if [ -e "./MinecraftClient" -a -n "$MCC_SKIP_REDOWNLOAD" ]; then
   echo "Skip re-download MinecraftClient"
 else
   # Use the provided version tag or get the latest release tag
-  RELEASE_TAG=${MCC_VERSION:-$(curl -s -v https://github.com/MCCTeam/Minecraft-Console-Client/releases/latest 2>&1 | grep -i location: | tr -d '\r' | cut -d/ -f8)}
+  RELEASE_TAG=${MCC_VERSION:-$(curl -s -v https://github.com/Dellism/Minecraft-Console-Client/releases/latest 2>&1 | grep -i location: | tr -d '\r' | cut -d/ -f8)}
 
   # Taken from https://stackoverflow.com/a/70369688
   ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/x64/)
@@ -16,10 +16,10 @@ else
   # Delete the old build
   [ -e MinecraftClient ] && rm -- MinecraftClient
 
-  echo "Donwloading MinecraftClient for ${RELEASE_TAG}-${ARCH}"
+  echo "Downloading MinecraftClient for ${RELEASE_TAG}-${ARCH}"
 
   # Download the specified build or the latest one
-  curl -L https://github.com/MCCTeam/Minecraft-Console-Client/releases/download/${RELEASE_TAG}/MinecraftClient-${RELEASE_TAG}-${MCC_PLATFORM:=linux}-${ARCH} --output MinecraftClient
+  curl -L https://github.com/Dellism/Minecraft-Console-Client/releases/download/${RELEASE_TAG}/MinecraftClient-${RELEASE_TAG}-${MCC_PLATFORM:=linux}-${ARCH} --output MinecraftClient
 fi
 
 # Set Executable
